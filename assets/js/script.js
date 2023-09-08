2154b7cf208cea716475f11a108d68c786a834e8
Qohelet <benjy.shuter@gmail.com>
2023-09-08 03:29:48 -0400

    Adds loading page with full functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -505,7 +505,7 @@ function renderEmojis(){
         for (let i=0; i<choiceSlots.length; i++){
             choiceSlots[i].removeEventListener("click", removeEmoji);
         };
-        const loadTest =  await new Promise(resolve => setTimeout(resolve, 5000));
+        // const loadTest =  await new Promise(resolve => setTimeout(resolve, 5000));
 
         for (let i=0; i<chosenEmojis.length; i++){
             const wait1 = await wordParser(chosenEmojis[i].name);
@@ -517,7 +517,7 @@ function renderEmojis(){
         // console.log(keywordsRaw)
         // console.log(keywordsTrash)
         isLoading = false;
-        // stageUpFn();
+        stageUpFn();
     };
 
     function renderEmojiGrid(){
@@ -747,49 +747,49 @@ function renderLoad(){
     setTimeout(()=>{
         if (isLoading){
             baseCard.innerHTML="";
-            // let h3 = document.createElement("h3");
-            let waitDiv = document.createElement("div");
-            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            // let svg = document.createElement("svg");
-            let path1 = document.createElement("path");
-            let path2 = document.createElement("path");
-            let span = document.createElement("span");
-
-            waitDiv.setAttribute("role", "status");
-
-            svg.setAttribute("aria-hidden", "true");
-            svg.setAttribute("class", "inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600");
-            // svg.setAttributeNS("", "viewBox", "0 0 100 101");
-            svg.setAttribute("viewBox", "0 0 100 101");
-            svg.setAttribute("fill", "none");
-            svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-
-            path1.setAttribute("d", "M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z");
-            path1.setAttribute("fill", "currentColor");
-
-            path2.setAttribute("d", "M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z");
-            path2.setAttribute("fill", "currentFill");
-
-            span.setAttribute("class", "sr-only");
-            span.textContent = "Loading..."
+            let loadCounter = 4;
+            let loadDiv = document.createElement("div");
+            let staticDiv = document.createElement("div");
+            let p = document.createElement("p");
+            // let span = document.createElement("span");
 
 
-            // <div role="status">
-            //     <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            //         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            //         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-            //     </svg>
-            //     <span class="sr-only">Loading...</span>
-            // </div>
+            loadDiv.setAttribute("class", "loadDiv");
+            staticDiv.setAttribute("class", "staticDiv");
 
-            svg.appendChild(path1);
-            svg.appendChild(path2);
+            // p.textContent = "Loading";
+            // span.textContent = ""
+            loadText()
+            loadTimer()
 
-            waitDiv.appendChild(svg);
-            waitDiv.appendChild(span);
 
-            baseCard.appendChild(waitDiv);
-        }
+            staticDiv.appendChild(p);
+            loadDiv.appendChild(staticDiv);
+            // loadDiv.appendChild(span);
+            baseCard.appendChild(loadDiv);
+
+            function loadTimer(){
+                let timer = setInterval(()=>{
+                    if (isLoading===false){
+                        clearInterval(timer);
+                    } else{
+                        loadText();
+                    };
+                }, 500);
+            };
+            function loadText(){
+                let baseText = "Loading";
+                if (loadCounter<4){
+                    for (let i=0; i<loadCounter; i++){
+                        baseText+=".";
+                    };
+                    loadCounter++;
+                } else {
+                    loadCounter = 1;
+                };
+                p.textContent = baseText;
+            };
+        };
     }, 500);
 };
 
